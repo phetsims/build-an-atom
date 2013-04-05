6cf31882469ab5da7a18d6f8411bb46209f98e2f
jbphet <john.blanco@colorado.edu>
2013-04-05 16:09:54 -0600

    Added a stage size.

--- js/common/SharedConstants.js
+++ js/common/SharedConstants.js
@@ -4,8 +4,12 @@ define( [ ], function () {
   // Not meant to be instantiated.
   var SharedConstants = {};
 
-  SharedConstants.NUCLEON_RADIUS = 12; // In pixels.
-  SharedConstants.ELECTRON_RADIUS = 10; // In pixels.
+  // Size of the stage, in screen coordinates.  This was obtained by setting
+  // a Chrome window to 1024 x 768 and measuring the actual display region.
+  SharedConstants.STAGE_SIZE = { width : 1010, height: 655 };
+
+  SharedConstants.NUCLEON_RADIUS = 12; // In screen coordinates, which are roughly pixels.
+  SharedConstants.ELECTRON_RADIUS = 10; // In screen coordinates, which are roughly pixels.
 
   return SharedConstants;
 
