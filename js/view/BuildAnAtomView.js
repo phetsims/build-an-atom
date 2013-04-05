1e182c9f8e087df995afb839dc60d14b1c89740c
jbphet <john.blanco@colorado.edu>
2013-04-05 12:48:27 -0600

    Removed a bunch of prototyping code.

--- js/view/BuildAnAtomView.js
+++ js/view/BuildAnAtomView.js
@@ -3,7 +3,6 @@
 /**
  * Main view for the first tab of the Build an Atom simulation.
  */
-
 define( function ( require ) {
   "use strict";
 
@@ -20,17 +19,24 @@ define( function ( require ) {
   var Bucket = require( 'PHETCOMMON/model/Bucket' ); // TODO: Only needed temporarily, I think.
   var Vector2 = require( 'DOT/Vector2' );
 
+  /**
+   * Constructor.
+   *
+   * @param model Build an Atom model object.
+   * @constructor
+   */
   function BuildAnAtomView( model ) {
     var view = this;
 
     // Create the scene where the user will construct the atoms.
     var sceneGraphDiv = $( '#scene-graph' );
 
-    // Initialize our scene
+    // Initialize the scene.
     var scene = new Scene( sceneGraphDiv );
     scene.initializeFullscreenEvents(); // sets up listeners on the document with preventDefault(), and forwards those events to our scene
     scene.resizeOnWindowResize(); // the scene gets resized to the full screen size
 
+    // Add a root node.  TODO: Review with JO - Do I want or need this? Where did option values come from and are they correct?
     var rootNode = new Node( {
                                x: scene.sceneBounds.width / 3,
                                y: scene.sceneBounds.centerY,
@@ -39,24 +45,8 @@ define( function ( require ) {
                              } );
     scene.addChild( rootNode );
 
-    // keep the container centered
-    function layout() {
-      rootNode.x = scene.sceneBounds.width / 3;
-      rootNode.y = scene.sceneBounds.centerY;
-    }
-
-    $( window ).resize( layout );
-
-    // Create a particle.
-//      var testParticle = new Particle();
-//      var testParticle = Particle.createProton();
-    var testParticle = Particle.createNeutron();
-
     // Create the model-view transform. TODO: This is just using numbers for now, will need to make this more dynamic.  Or something.  Not at all sure.
     var mvt = new ModelViewTransform2D( 1, { x: 200, y: 200 } );
-
-    // Create a particle view.
-    rootNode.addChild( new ParticleView( testParticle, mvt ) );
 
     // Add the front portion of the buckets.  Done separately from the bucket
     // holes for layering purposes.
@@ -69,47 +59,14 @@ define( function ( require ) {
       rootNode.addChild( new ParticleView( nucleon, mvt ) );
     });
 
-
-    // our paths node for the shape
-    var protonView = new Path( {
-                                 shape: Shape.circle( 0, 0, 40 ),
-                                 fill: 'red',
-                                 stroke: '#000000',
-                                 cursor: 'pointer'
-                               } );
-
-    var neutronView = new Path( {
-                                  shape: Shape.circle( 0, 0, 40 ),
-                                  fill: 'gray',
-                                  stroke: '#000000',
-                                  cursor: 'pointer'
-                                } );
-    var count = 0;
-    for ( var i = 0; i < count; i++ ) {
-      // anonymous function so the closure variables aren't shared between iterations
-      (function () {
-        var node = new Node( {
-                               x: ( Math.random() - 0.5 ) * 300,
-                               y: ( Math.random() - 0.5 ) * 300
-                             } );
-
-        // point each node to the same path (DAG)
-        node.addChild( Math.random() < 0.5 ? protonView : neutronView );
-        rootNode.addChild( node );
-
-        // add a drag handler to each node
-        node.addInputListener( new SimpleDragHandler( {
-                                                        // allow moving a finger (touch) across a node to pick it up
-                                                        allowTouchSnag: true
-                                                      } ) );
-      })();
-
-
+    // Set up a callback that will keep the scene centered.
+    function layout() {
+      rootNode.x = scene.sceneBounds.width / 3;
+      rootNode.y = scene.sceneBounds.centerY;
     }
+    $( window ).resize( layout );
 
-    var lastTime = 0;
-    var timeElapsed = 0;
-
+    // Create tick function for animation.
     function tick() {
       window.requestAnimationFrame( tick, sceneGraphDiv[0] );
 
@@ -119,41 +76,6 @@ define( function ( require ) {
     }
 
     window.requestAnimationFrame( tick, sceneGraphDiv[0] );
-
-
-    // Add the bucket holes to the scene.
-//  rootNode.addChild( new BucketHole( buildAnAtomModel.buckets.protonBucket, mvt ) );
-//  rootNode.addChild( new BucketHole( buildAnAtomModel.buckets.neutronBucket, mvt ) );
-//  rootNode.addChild( new BucketHole( buildAnAtomModel.buckets.electronBucket, mvt ) );
-
-    // Add the bucket fronts to the scene.
-//      rootNode.addChild( new BucketFront( buildAnAtomModel.buckets.protonBucket, mvt ) );
-//      rootNode.addChild( new BucketFront( buildAnAtomModel.buckets.neutronBucket, mvt ) );
-//      rootNode.addChild( new BucketFront( buildAnAtomModel.buckets.electronBucket, mvt ) );
-
-
-//      var bucket = new Bucket( {
-//                                 x: 600,
-//                                 y: 100,
-//                                 width: 200,
-//                                 height: 50,
-//                                 baseColor: 'red',
-//                                 caption: 'Protons'
-//                               } );
-//      rootNode.addChild( new BucketFront( bucket ) );
-
-    /*---------------------------------------------------------------------------*
-     * FPS meter
-     *----------------------------------------------------------------------------*/
-
-    //    var stats = new Stats();
-    //    stats.setMode( 0 ); // 0: fps, 1: ms
-    //    stats.domElement.style.position = 'absolute';
-    //    stats.domElement.style.right = '0';
-    //    stats.domElement.style.top = '0';
-    //    document.body.appendChild( stats.domElement );
-
-
   }
 
 
