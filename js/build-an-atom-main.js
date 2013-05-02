9ee873c163d3aecccc37579ab3d1e59cfa3cc819
jbphet <john.blanco@colorado.edu>
2013-05-02 15:18:14 -0600

    Added some quick icons for the 1st and 2nd tabs.

--- js/build-an-atom-main.js
+++ js/build-an-atom-main.js
@@ -5,26 +5,69 @@ require(
       'buildanatom/view/BuildAnAtomView',
       'symbol/model/SymbolModel',
       'symbol/view/SymbolTabView',
+      'SCENERY/nodes/Circle',
       'SCENERY/nodes/Rectangle' ,
+      'SCENERY/nodes/Text' ,
       'JOIST/Sim'
     ],
-    function ( BuildAnAtomModel, BuildAnAtomView, SymbolModel, SymbolTabView, Rectangle, Sim ) {
+    function( BuildAnAtomModel, BuildAnAtomView, SymbolModel, SymbolTabView, Circle, Rectangle, Text, Sim ) {
       "use strict";
+
+      var icon1 = new Rectangle( 0, 0, 50, 50, {fill: 'rgb(255, 254, 223)'} );
+      icon1.addChild( new Circle( 10,
+                                  {
+                                    stroke: 'blue',
+                                    lineWidth: 0.5,
+                                    lineDash: [ 1, 1 ],
+                                    translation: {x: 25, y: 25 }
+                                  }
+      ) );
+      icon1.addChild( new Circle( 20,
+                                  {
+                                    stroke: 'blue',
+                                    lineWidth: 0.5,
+                                    lineDash: [ 1, 1 ],
+                                    translation: {x: 25, y: 25 }
+                                  }
+      ) );
+
+      var icon2 = new Rectangle( 0, 0, 50, 50, {fill: 'rgb(255, 254, 223)'} );
+      icon2.addChild( new Rectangle( 0, 0, 30, 30,
+                                  {
+                                    stroke: 'black',
+                                    lineWidth: 0.5,
+                                    translation: {x: 10, y: 10 },
+                                    fill: 'rgb( 255, 255, 255)'
+                                  }
+      ) );
+      icon2.addChild( new Text( "H", {
+        translation: { x: 17, y: 30 },
+        font: "20px Arial"
+      }));
+
 
       //Create and start the sim
       //TODO: i18n
       new Sim( "Build an Atom", [
         { name: "Build an Atom",
-          icon: new Rectangle( 0, 0, 50, 50, {fill: 'blue'} ),
+          icon: icon1,
           backgroundColor: 'rgb(255, 254, 223)',
-          createModel: function () {return new BuildAnAtomModel();},
-          createView: function ( model ) {return new BuildAnAtomView( model ).scene;}
+          createModel: function() {
+            return new BuildAnAtomModel();
+          },
+          createView: function( model ) {
+            return new BuildAnAtomView( model ).scene;
+          }
         },
         { name: "Symbol",
-          icon: new Rectangle( 0, 0, 50, 50, {fill: 'red'} ),
+          icon: icon2,
           backgroundColor: 'rgb(255, 254, 223)',
-          createModel: function () {return new SymbolModel();},
-          createView: function ( model ) {return new SymbolTabView( model ).scene;}
+          createModel: function() {
+            return new SymbolModel();
+          },
+          createView: function( model ) {
+            return new SymbolTabView( model ).scene;
+          }
         }
       ], { home: true, tab: 0, navigationBarInFront: true} ).start();
     } );
