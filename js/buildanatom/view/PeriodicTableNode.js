d8e05a8bbb0398ee54d158a8fb8213dcc92a3d84
jbphet <john.blanco@colorado.edu>
2013-05-13 18:08:19 -0600

    Started on implementation of Scenery-based periodic table.

--- js/buildanatom/view/PeriodicTableNode.js
+++ js/buildanatom/view/PeriodicTableNode.js
@@ -1,40 +1,57 @@
-define( [
-          '../../../.',
-          'common/AtomIdentifier',
-          'tpl!templates/periodic-table.html'
-        ], function ( _, AtomIdentifier, periodicTableTemplate ) {
+define( function( require ) {
+
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var HTMLText = require( 'SCENERY/nodes/HTMLText' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Vector2 = require( 'DOT/Vector2' );
+  var AtomIdentifier = require( 'common/AtomIdentifier' );
+  var periodicTableTemplate = require( 'tpl!templates/periodic-table.html' );
 
   function PeriodicTableNode( atom ) {
+    Node.call( this ); // Call super constructor.
     this.atom = atom;
 
     if ( !this.atom ) {
-      throw new Error( 'I need an atom!' );
+      throw new Error( 'Periodic table must be constructed with an atom.' );
     }
 
-    this.$el = $( '#periodic-table' );
-    this.el = this.$el[0];
+    // Add the bounding box, which is also the root node for everything else
+    // that comprises this node.
+    var boundingBox = new Rectangle( 0, 0, 100, 100, 0, 0,
+                                     {
+                                       stroke: 'black',
+                                       lineWidth: 2,
+                                       fill: 'white'
+                                     } );
+    this.addChild( boundingBox );
 
-    var self = this;
-    var numProtons = this.atom.getNumProtons();
+    this.addChild( new HTMLText( periodicTableTemplate ) );
 
-    this.atom.events.on( 'configurationChanged', function () {
-      var newNumProtons = self.atom.getNumProtons();
+    var numProtons = this.atom.protonCount;
 
-      if ( numProtons !== newNumProtons ) {
-        numProtons = newNumProtons;
-        self.render();
-      }
-    } );
-
-    this.$el.html( periodicTableTemplate( {} ) );
-    this.render();
+//    this.atom.protons( 'configurationChanged', function () {
+//      var newNumProtons = self.atom.getNumProtons();
+//
+//      if ( numProtons !== newNumProtons ) {
+//        numProtons = newNumProtons;
+//        self.render();
+//      }
+//    } );
+//
+//    this.$el.html( periodicTableTemplate( {} ) );
+//    this.render();
   }
 
-  PeriodicTableNode.prototype.render = function () {
-    var symbol = AtomIdentifier.getSymbol( this.atom.getNumProtons() );
-    this.$el.find( '.active' ).removeClass( 'active' );
-    this.$el.find( '[data-symbol="' + symbol + '"]' ).addClass( 'active' );
-  };
+//  PeriodicTableNode.prototype.render = function () {
+//    var symbol = AtomIdentifier.getSymbol( this.atom.getNumProtons() );
+//    this.$el.find( '.active' ).removeClass( 'active' );
+//    this.$el.find( '[data-symbol="' + symbol + '"]' ).addClass( 'active' );
+//  };
+
+  // Inherit from Node.
+  inherit( PeriodicTableNode, Node );
 
   return PeriodicTableNode;
 
