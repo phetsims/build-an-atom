0ec916c9b69a452fabf494b2204162e60e9a7b20
jbphet <john.blanco@colorado.edu>
2013-05-13 18:21:45 -0600

    Added constants for controlling cell structure.

--- js/buildanatom/view/PeriodicTableNode.js
+++ js/buildanatom/view/PeriodicTableNode.js
@@ -6,8 +6,26 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Vector2 = require( 'DOT/Vector2' );
+  var Dimension2 = require( 'DOT/Dimension2' );
   var AtomIdentifier = require( 'common/AtomIdentifier' );
   var periodicTableTemplate = require( 'tpl!templates/periodic-table.html' );
+
+  // Constants that define the table structure.
+  var ELEMENT_COLUMNS = 18;
+//  var ELEMENT_ROWS = 7;
+//  var POPULATED_CELLS_IN_FIRST_ROW = [0, 17];
+//  var POPULATED_CELLS_IN_SECOND_ROW = [0, 1, 12, 13, 14, 15, 16, 17];
+//  var POPULATED_CELLS_IN_THIRD_ROW = [0, 1, 12, 13, 14, 15, 16, 17];
+  var POPULATED_CELLS = [
+    [ 0, 17 ],
+    [0, 1, 12, 13, 14, 15, 16, 17],
+    [0, 1, 12, 13, 14, 15, 16, 17],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  ];
+  var CELL_SIZE = new Dimension2( 40, 40 );
 
   function PeriodicTableNode( atom ) {
     Node.call( this ); // Call super constructor.
@@ -19,7 +37,7 @@ define( function( require ) {
 
     // Add the bounding box, which is also the root node for everything else
     // that comprises this node.
-    var boundingBox = new Rectangle( 0, 0, 100, 100, 0, 0,
+    var boundingBox = new Rectangle( 0, 0, CELL_SIZE.width, CELL_SIZE.height, 0, 0,
                                      {
                                        stroke: 'black',
                                        lineWidth: 2,
