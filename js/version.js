6fdd10b26d51c3688808b4d77ef7b45f3e72237d
jbphet <john.blanco@colorado.edu>
2013-05-13 18:07:26 -0600

    Added version.js file for compatibility with framework.

---
+++ js/version.js
@@ -0,0 +1,9 @@
+// Copyright 2013, University of Colorado
+
+/**
+ * Returns version identifier for the simulation.
+ * TODO: generate this file a build time, reading package.json
+ */
+define( function( require ) {
+  return function() { return "0.0.0.1"; }
+} );
