bfd743a9b765a32c788929d32f7769e5b2aa27c1
jbphet <john.blanco@colorado.edu>
2013-05-23 13:23:23 -0600

    Added reset button.  Not hooked up yet.

--- js/symbol/view/SymbolTabView.js
+++ js/symbol/view/SymbolTabView.js
@@ -40,13 +40,14 @@ define( function( require ) {
 
     // Add the atom display.
     console.log( "periodicTableNode.maxX + 20 = " + periodicTable.right + 20 );
-    this.addChild( new AtomWithParticleStacks( model ).mutate( {
+    var atomView = new AtomWithParticleStacks( model ).mutate( {
                                                                  left: periodicTable.right + 40,
-                                                                 top: symbolNode.minY
-                                                               } ) );
+                                                                 top: 50
+                                                               } );
+    this.addChild( atomView );
 
     // Add the reset button. TODO: i18n
-    this.addChild( new Button( new Text( "Reset", { font: 'bold 24px Arial'} ),
+    this.addChild( new Button( new Text( "Reset", { font: 'bold 16px Arial'} ),
                                function() {
                                  model.reset();
                                },
@@ -54,7 +55,7 @@ define( function( require ) {
                                  fill: 'orange',
                                  xMargin: 10,
                                  lineWidth: 1.5
-                               } ).mutate( {center: new Vector2( 900, 650 )} ) );
+                               } ).mutate( {center: new Vector2( atomView.centerX, atomView.bottom + 40 )} ) );
   }
 
   // Inherit from TabView.
