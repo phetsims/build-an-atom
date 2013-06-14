ded063ba2f41517119dbecc1cf5ae499d819fd52
jbphet <john.blanco@colorado.edu>
2013-06-14 17:45:18 -0600

    Removed lodash from the module system, it is now assumed to be part of the operating environment.

--- js/build-an-atom-config.js
+++ js/build-an-atom-config.js
@@ -8,7 +8,6 @@ require.config(
     paths: {
       // local lib dependencies
       i18n: "../lib/i18n-2.0.2",
-      lodash: "../lib/lodash-1.0.1",
 
       // PhET's common code, uppercase names to identify them in require imports
       PHETCOMMON: "../../phetcommon/js",
@@ -24,10 +23,6 @@ require.config(
       FORT: '../../fort/js',
       JOIST: '../../joist/js',
       SUN: '../../sun/js'
-    },
-
-    shim: {
-      lodash: { exports: "_" }
     },
 
     urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
