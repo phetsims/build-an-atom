07737cdf210f94f3990cbc32723d71e6e1588467
jbphet <john.blanco@colorado.edu>
2013-07-01 13:57:40 -0600

    Added the particle counts.

--- js/game/view/CountsToElementProblemView.js
+++ js/game/view/CountsToElementProblemView.js
@@ -21,7 +21,8 @@ define( function( require ) {
   var Text = require( 'SCENERY/nodes/Text' );
 
   // Constants
-  var FONT = new BAAFont( 30 );
+  var TITLE_FONT = new BAAFont( 30 );
+  var PARTICLE_COUNTS_FONT = new BAAFont( 24 );
   var INSET = 10;
 
   /**
@@ -38,13 +39,27 @@ define( function( require ) {
       console.log( "Error: Layout bounds must start at 0, 0" );
     }
 
+    // Particle counts
+    var protonCountTitle = new Text( "Protons:", PARTICLE_COUNTS_FONT );
+    this.addChild( protonCountTitle );
+    var protonCountText = new Text( answerAtom.protonCount, PARTICLE_COUNTS_FONT );
+    this.addChild( protonCountText );
+    var neutronCountTitle = new Text( "Neutrons:", PARTICLE_COUNTS_FONT );
+    this.addChild( neutronCountTitle );
+    var neutronCountText = new Text( answerAtom.neutronCount, PARTICLE_COUNTS_FONT );
+    this.addChild( neutronCountText );
+    var electronCountTitle = new Text( "Electrons:", PARTICLE_COUNTS_FONT );
+    this.addChild( electronCountTitle );
+    var electronCountText = new Text( answerAtom.electronCount, PARTICLE_COUNTS_FONT );
+    this.addChild( electronCountText );
+
     // Periodic table
     var periodicTableAtom = new NumberAtom();
     var periodicTable = new PeriodicTableNode( periodicTableAtom, 100 );
     this.addChild( periodicTable );
 
     // Problem title
-    var problemTitle = new Text( "Find the element:", { font: FONT } ); // TODO: i18n
+    var problemTitle = new Text( "Find the element:", { font: TITLE_FONT } ); // TODO: i18n
     this.addChild( problemTitle );
 
     // Layout
@@ -57,6 +72,22 @@ define( function( require ) {
     problemTitle.centerX = periodicTable.centerX;
     problemTitle.bottom = periodicTable.top - 30; // Offset empirically determined.
 
+    var countIndicatorRightEdge = layoutBounds.width * 0.25; // Controls horizontal position of count indicators, adjust as needed.
+    protonCountText.right = countIndicatorRightEdge;
+    neutronCountText.right = protonCountText.right;
+    electronCountText.right = protonCountText.right;
+    protonCountText.centerY = periodicTable.top + periodicTable.height * 0.25;
+    neutronCountText.centerY = periodicTable.centerY;
+    electronCountText.centerY = periodicTable.top + periodicTable.height * 0.75;
+    var maxNumberWidth = new Text( "99", { font: PARTICLE_COUNTS_FONT } ).width;
+    var maxParticleLabelWidth = Math.max( Math.max( protonCountTitle.width, neutronCountTitle.width ), electronCountTitle.width );
+    var countTitleLeftEdge = countIndicatorRightEdge - maxNumberWidth - maxParticleLabelWidth - 10;
+    protonCountTitle.left = countTitleLeftEdge;
+    neutronCountTitle.left = countTitleLeftEdge;
+    electronCountTitle.left = countTitleLeftEdge;
+    protonCountTitle.centerY = protonCountText.centerY;
+    neutronCountTitle.centerY = neutronCountText.centerY;
+    electronCountTitle.centerY = electronCountText.centerY;
   }
 
   // Inherit from Node.
