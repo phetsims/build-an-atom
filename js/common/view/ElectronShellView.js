e6a3e821fb9ac90f11d92cbe7e8fe2e36ebfc434
jbphet <john.blanco@colorado.edu>
2013-07-09 11:30:25 -0600

    Changed rendered for shells to SVG to fix issue where IE shows solid instead of dashed lines.  Also adjusted line parameter to make it look a bit better.

--- js/common/view/ElectronShellView.js
+++ js/common/view/ElectronShellView.js
@@ -2,11 +2,15 @@
 define( function( require ) {
   "use strict";
 
+  // Imports
   var Node = require( 'SCENERY/nodes/Node' );
   var Circle = require( 'SCENERY/nodes/Circle' );
   var RadialGradient = require( 'SCENERY/util/RadialGradient' );
   var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
   var inherit = require( 'PHET_CORE/inherit' );
+
+  // Constants
+  var LINE_DASH = [ 4, 5 ];
 
   var ElectronShellView = function ElectronShellView( atom, mvt ) {
     Node.call( this ); // Call super constructor.
@@ -15,9 +19,10 @@ define( function( require ) {
     var outerRing = new Circle( outerRadiusInView,
       // Options
                                 {
+                                  renderer: 'svg', // This is necessary to get dotted lines on IE10, see Scenery issue #70.
                                   stroke: 'blue',
                                   lineWidth: 1.5,
-                                  lineDash: [ 4, 6 ],
+                                  lineDash: LINE_DASH,
                                   translation: mvt.modelToViewPosition( {x: 0, y: 0 } )
                                 }
     );
@@ -27,9 +32,10 @@ define( function( require ) {
     var innerRing = new Circle( innerRadiusInView,
       // Options
                                 {
+                                  renderer: 'svg', // This is necessary to get dotted lines on IE10, see Scenery issue #70.
                                   stroke: 'blue',
                                   lineWidth: 1.5,
-                                  lineDash: [ 4, 6 ],
+                                  lineDash: LINE_DASH,
                                   translation: mvt.modelToViewPosition( {x: 0, y: 0 } )
                                 }
     );
