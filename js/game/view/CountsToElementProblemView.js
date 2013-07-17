c959b394af6c0a06dc95882f17694f6afdb6a540
jbphet <john.blanco@colorado.edu>
2013-07-17 15:20:56 -0600

    Pulled the particle counts into a separate, reusable node.

--- js/game/view/CountsToElementProblemView.js
+++ js/game/view/CountsToElementProblemView.js
@@ -12,6 +12,7 @@ define( function( require ) {
 
   // Imports
   var BAAFont = require( 'common/view/BAAFont' );
+  var ParticleCountsNode = require( 'game/view/ParticleCountsNode' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var ToElementProblemView = require( 'game/view/ToElementProblemView' );
@@ -30,37 +31,12 @@ define( function( require ) {
     var thisNode = this;
 
     // Particle counts
-    var protonCountTitle = new Text( 'Protons:', PARTICLE_COUNTS_FONT );
-    this.problemPresentationNode.addChild( protonCountTitle );
-    var protonCountText = new Text( countsToElementProblem.answerAtom.protonCount, PARTICLE_COUNTS_FONT );
-    this.problemPresentationNode.addChild( protonCountText );
-    var neutronCountTitle = new Text( 'Neutrons:', PARTICLE_COUNTS_FONT );
-    this.problemPresentationNode.addChild( neutronCountTitle );
-    var neutronCountText = new Text( countsToElementProblem.answerAtom.neutronCount, PARTICLE_COUNTS_FONT );
-    this.problemPresentationNode.addChild( neutronCountText );
-    var electronCountTitle = new Text( 'Electrons:', PARTICLE_COUNTS_FONT );
-    this.problemPresentationNode.addChild( electronCountTitle );
-    var electronCountText = new Text( countsToElementProblem.answerAtom.electronCount, PARTICLE_COUNTS_FONT );
-    this.problemPresentationNode.addChild( electronCountText );
+    var particleCountsNode = new ParticleCountsNode( countsToElementProblem.answerAtom );
+    this.problemPresentationNode.addChild( particleCountsNode );
 
-    //--------------------------- Layout -------------------------------------
-
-    var countIndicatorRightEdge = layoutBounds.width * 0.35; // Controls horizontal position of count indicators, adjust as needed.
-    protonCountText.right = countIndicatorRightEdge;
-    neutronCountText.right = protonCountText.right;
-    electronCountText.right = protonCountText.right;
-    protonCountText.centerY = this.periodicTable.top + this.periodicTable.height * 0.25;
-    neutronCountText.centerY = this.periodicTable.centerY;
-    electronCountText.centerY = this.periodicTable.top + this.periodicTable.height * 0.75;
-    var maxNumberWidth = new Text( '99', { font: PARTICLE_COUNTS_FONT } ).width;
-    var maxParticleLabelWidth = Math.max( Math.max( protonCountTitle.width, neutronCountTitle.width ), electronCountTitle.width );
-    var countTitleLeftEdge = countIndicatorRightEdge - maxNumberWidth - maxParticleLabelWidth - 10;
-    protonCountTitle.left = countTitleLeftEdge;
-    neutronCountTitle.left = countTitleLeftEdge;
-    electronCountTitle.left = countTitleLeftEdge;
-    protonCountTitle.centerY = protonCountText.centerY;
-    neutronCountTitle.centerY = neutronCountText.centerY;
-    electronCountTitle.centerY = electronCountText.centerY;
+    // Layout
+    particleCountsNode.centerX = layoutBounds.width * 0.25;
+    particleCountsNode.centerY = this.periodicTable.centerY;
   }
 
   // Inherit from ProblemView.
---
+++ js/game/view/ParticleCountsNode.js
@@ -0,0 +1,59 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * Node that takes a number atom and displays a set of counts for the various
+ * subatomic particles.  This is generally used when presenting a 'problem'
+ * for the game.
+ */
+define( function( require ) {
+  'use strict';
+
+  // Imports
+  var BAAFont = require( 'common/view/BAAFont' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var inherit = require( 'PHET_CORE/inherit' );
+
+  function ParticleCountsNode( numberAtom, options ) {
+
+    Node.call( this, options ); // Call super constructor.
+
+    options = _.extend( { font: new BAAFont( 24 )}, options );
+
+    // TODO: i18n
+    var protonCountTitle = new Text( 'Protons:', options.font );
+    this.addChild( protonCountTitle );
+    var protonCountText = new Text( numberAtom.protonCount, options.font );
+    this.addChild( protonCountText );
+    var neutronCountTitle = new Text( 'Neutrons:', options.font );
+    this.addChild( neutronCountTitle );
+    var neutronCountText = new Text( numberAtom.neutronCount, options.font );
+    this.addChild( neutronCountText );
+    var electronCountTitle = new Text( 'Electrons:', options.font );
+    this.addChild( electronCountTitle );
+    var electronCountText = new Text( numberAtom.electronCount, options.font );
+    this.addChild( electronCountText );
+
+    // Layout - Line labels up on left edge, numbers on right edge.
+    var maxParticleLabelWidth = Math.max( Math.max( protonCountTitle.width, neutronCountTitle.width ), electronCountTitle.width );
+    var interLineSpacing = protonCountTitle.height * 0.7; // Multiplier empirically determined.
+    var numberAreaWidth = new Text( '000', { font: options.font } ).width;
+    protonCountTitle.left = 0;
+    protonCountTitle.top = 0;
+    protonCountText.bottom = protonCountTitle.bottom;
+    protonCountText.right = maxParticleLabelWidth + numberAreaWidth;
+    neutronCountTitle.left = 0;
+    neutronCountTitle.top = protonCountTitle.bottom + interLineSpacing;
+    neutronCountText.bottom = neutronCountTitle.bottom;
+    neutronCountText.right = maxParticleLabelWidth + numberAreaWidth;
+    electronCountTitle.left = 0;
+    electronCountTitle.top = neutronCountTitle.bottom + interLineSpacing;
+    electronCountText.bottom = electronCountTitle.bottom;
+    electronCountText.right = maxParticleLabelWidth + numberAreaWidth;
+  }
+
+  // Inherit from Node.
+  inherit( Node, ParticleCountsNode );
+
+  return ParticleCountsNode;
+} );
