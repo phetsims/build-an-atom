c2ff11f7fba2d2707248924fa11a52b485e48068
Chris Malley <cmalley@pixelzoom.com>
2013-07-26 15:06:15 -0600

    order PhET libs to match package.json

--- js/build-an-atom-config.js
+++ js/build-an-atom-config.js
@@ -6,21 +6,20 @@ require.config(
     deps: ['build-an-atom-main'],
 
     paths: {
-      // local lib dependencies
+
+      // third-party libs
       i18n: '../lib/i18n-2.0.2',
 
-      // PhET's common code, uppercase names to identify them in require imports
-      PHETCOMMON: '../../phetcommon/js',
-
-      // Scenery and its dependencies
+       // PhET libs, uppercase names to identify them in require.js imports
       ASSERT: '../../assert/js',
       AXON: '../../axon/js',
       DOT: '../../dot/js',
+      JOIST: '../../joist/js',
       KITE: '../../kite/js',
+      PHETCOMMON: '../../phetcommon/js',
       PHET_CORE: '../../phet-core/js',
       SCENERY: '../../scenery/js',
       SCENERY_PHET: '../../scenery-phet/js',
-      JOIST: '../../joist/js',
       SUN: '../../sun/js'
     },
 
