4e1cc7d0a50d5db062225434adbf582ffcaccd1c
Jonathan Olson <olsonsjc@gmail.com>
2013-07-31 17:09:34 -0600

    Particle view cache-like performance testing

--- js/common/view/ParticleView.js
+++ js/common/view/ParticleView.js
@@ -7,14 +7,43 @@ define( function( require ) {
   'use strict';
 
   var Node = require( 'SCENERY/nodes/Node' );
+  var Image = require( 'SCENERY/nodes/Image' );
   var Circle = require( 'SCENERY/nodes/Circle' );
   var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
   var ParticleNode = require( 'common/view/ParticleNode' );
   var inherit = require( 'PHET_CORE/inherit' );
 
+  var particleNodes = {};
+
+  function getParticleNode( particle, mvt ) {
+    var id = 'id-' + particle.type + '-' + particle.radius;
+    if ( particleNodes[id] ) {
+      return particleNodes[id];
+    }
+
+    //Scale up before rasterization so it won't be too pixellated/fuzzy
+    var scale = 2;
+    // var particleNode = new ParticleNode( particle.type, mvt.modelToViewDeltaX( particle.radius ) ).mutate( { scale: scale } );
+    var viewRadius = mvt.modelToViewDeltaX( particle.radius );
+    var scaledRadius = viewRadius * scale;
+    
+    var particleNode = new ParticleNode( particle.type, scaledRadius );
+    console.log( scaledRadius );
+
+    var node = new Node( { cursor: 'pointer' } );
+    particleNode.toImage( function( im, x, y ) {
+
+      //Scale back down so the image will be the desired size
+      var image = new Image( im, { x: -x, y: -y } );
+      image.scale( 1 / scale, 1 / scale, true );
+      node.addChild( image );
+    }, scaledRadius + 1, scaledRadius + 1, 2 * ( scaledRadius + 1 ), 2 * ( scaledRadius + 1 ) );
+    return node;
+  }
+
   function ParticleView( particle, mvt ) {
 
-    Node.call( this, { renderer: 'svg', rendererOptions: { cssTransform: true } } ); // Call super constructor.
+    Node.call( this, { renderer: 'svg' } ); // Call super constructor.
     var thisParticleView = this;
 
     // Set up fields.
@@ -22,7 +51,7 @@ define( function( require ) {
     this.mvt = mvt;
 
     // Add the particle representation.
-    this.addChild( new ParticleNode( particle.type, mvt.modelToViewDeltaX( particle.radius ) ) );
+    this.addChild( getParticleNode( particle, mvt ) );
 
     // Listen to the model position and update.
     particle.positionProperty.link( function( position ) {
