04ea487220f7bb1049cd036544e919793c9e3ccb
samreid <reids@colorado.edu>
2013-09-19 09:11:22 -0600

    updated to use sherpa

--- js/build-an-atom-config.js
+++ js/build-an-atom-config.js
@@ -8,7 +8,7 @@ require.config(
     paths: {
 
       // third-party libs
-      i18n: '../lib/i18n-2.0.2',
+      i18n: '../../sherpa/i18n-2.0.4',
 
        // PhET libs, uppercase names to identify them in require.js imports
       ASSERT: '../../assert/js',
