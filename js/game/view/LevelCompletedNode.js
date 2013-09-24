16605af2c6263bbd8cfcada009cf87d6b2cb1b22
jbphet <john.blanco@colorado.edu>
2013-09-24 15:07:34 -0700

    Removed level indication and dividing lines from the 'level complete' screen, changed button to say 'continue', changed size.

--- js/game/view/LevelCompletedNode.js
+++ js/game/view/LevelCompletedNode.js
@@ -21,8 +21,7 @@ define( function( require ) {
 
   // Constants
   var BACKGROUND_COLOR = new Color( 180, 205, 255 );
-  var DIVIDER_LINE_OPTIONS = { stroke: 'black', lineWidth: 2 };
-  var INFO_TEXT_FONT = new PhetFont( 20 );
+  var INFO_TEXT_FONT = new PhetFont( { size: 22, weight: 'bold' } );
 
   /**
    * @param {BAAGameModel} gameModel
@@ -33,7 +32,7 @@ define( function( require ) {
 
     Node.call( this ); // Call super constructor.
 
-    var size = new Dimension2( layoutBounds.width * 0.33, layoutBounds.height * 0.6 );
+    var size = new Dimension2( layoutBounds.width * 0.5, layoutBounds.height * 0.5 );
 
     var rounding = size.width * 0.1;
     var background = new Rectangle( 0, 0, size.width, size.height, rounding, rounding,
@@ -61,52 +60,33 @@ define( function( require ) {
     title.scale( Math.min( 1, (size.width * 0.9 ) / title.width ) );
     background.addChild( title );
 
-    var dividerLineWidth = size.width * 0.8;
-    var upperDividerLine = new Line( 0, 0, dividerLineWidth, 0, DIVIDER_LINE_OPTIONS );
-    background.addChild( upperDividerLine );
-    var lowerDividerLine = new Line( 0, 0, dividerLineWidth, 0, DIVIDER_LINE_OPTIONS );
-    background.addChild( lowerDividerLine );
-
     // TODO: i18n of everything below
-    var level = new Text( 'Level: ' + ( gameModel.level + 1), { font: INFO_TEXT_FONT } );
-    background.addChild( level );
-    var scoreText = gameModel.scoreProperties[ gameModel.level ].value + ' of ' + gameModel.MAX_POINTS_PER_GAME_LEVEL;
-    if ( gameModel.scoreProperties[ gameModel.level ].value === gameModel.MAX_POINTS_PER_GAME_LEVEL ) {
-      scoreText += "(Perfect!)";
-    }
-    var score = new Text( 'Score: ' + gameModel.scoreProperties[ gameModel.level ].value + ' of ' + gameModel.MAX_POINTS_PER_GAME_LEVEL, { font: INFO_TEXT_FONT } );
+    var score = new Text( 'Score: ' + gameModel.scoreProperties[ gameModel.level ].value + ' out of ' + gameModel.MAX_POINTS_PER_GAME_LEVEL, { font: INFO_TEXT_FONT } );
     background.addChild( score );
     var time = new Text( 'Time: ' + Utils.formatTime( gameModel.elapsedTime ), { font: INFO_TEXT_FONT } );
     background.addChild( time );
 
-    var doneButton = new TextButton( 'OK',
+    var continueButton = new TextButton( 'Continue',
       function() {
         gameModel.state = 'selectSubGame';
       },
       { font: new PhetFont( 24 ), rectangleFillUp: new Color( 243, 243, 243 ) } );
-    background.addChild( doneButton );
+    background.addChild( continueButton );
 
     // Layout
     var inset = size.width * 0.05;
     title.centerX = background.width / 2;
     title.top = inset;
-    upperDividerLine.centerX = size.width / 2;
-    upperDividerLine.top = title.bottom + inset;
-    doneButton.centerX = size.width / 2;
-    doneButton.bottom = size.height - inset;
-    lowerDividerLine.centerX = size.width / 2;
-    lowerDividerLine.bottom = doneButton.top - inset;
-    level.left = upperDividerLine.left;
-    score.left = upperDividerLine.left;
-    time.left = upperDividerLine.left;
+    continueButton.centerX = size.width / 2;
+    continueButton.bottom = size.height - inset;
+    score.centerX = size.width / 2;
+    time.centerX = size.width / 2;
     if ( gameModel.timerEnabled ) {
-      level.centerY = size.height * 0.33;
       score.centerY = size.height * 0.5;
       time.centerY = size.height * 0.67;
     }
     else {
       time.visible = false;
-      level.centerY = size.height * 0.4;
       score.centerY = size.height * 0.6;
     }
   };
