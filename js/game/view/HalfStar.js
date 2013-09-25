27095ec33a041dd065bdec6a5bcd7c615c0901c2
jbphet <john.blanco@colorado.edu>
2013-09-24 17:01:40 -0700

    Made the half star unclosed on its side, looks better in the game progress indicator.

--- js/game/view/HalfStar.js
+++ js/game/view/HalfStar.js
@@ -16,11 +16,10 @@ define( function( require ) {
     var starShape = new Shape();
     var angle = -Math.PI / 2;
     for ( var i = 0; i < 6; i++ ) {
-      var vector = Vector2.createPolar( i % 2 === 0 ? diameter / 2 : diameter / 4, angle );
+      var vector = Vector2.createPolar( i % 2 === 0 ? diameter / 2 : diameter * 0.27, angle );
       starShape.lineTo( vector.x, vector.y );
       angle -= ( 2 * Math.PI ) / 10;
     }
-    starShape.close();
 
     Path.call( this, starShape, options ); // Call super constructor.
   };
