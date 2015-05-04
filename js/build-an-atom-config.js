4e0f3fa6eb5f43b799272007f19993360634fc4e
Jonathan Olson <olsonsjc@gmail.com>
2015-05-04 15:24:47 -0600

    Removing duplicate version.js (and adding REPOSITORY require.js prefix in config). See https://github.com/phetsims/chipper/issues/4

--- js/build-an-atom-config.js
+++ js/build-an-atom-config.js
@@ -22,6 +22,7 @@ require.config( {
     KITE: '../../kite/js',
     PHET_CORE: '../../phet-core/js',
     PHETCOMMON: '../../phetcommon/js',
+    REPOSITORY: '..',
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SHERPA: '../../sherpa',
--- js/version.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2002-2013, University of Colorado Boulder
-
-// Returns version identifier for the simulation.
-define( function( require ) {
-  'use strict';
-  return function() {
-    return '1.2.0-dev.1';
-  };
-} );
