8211a0f1444e08229e4fe88021a7b1afaf863cf8
samreid <reids@colorado.edu>
2015-11-06 20:50:41 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/view/InteractiveSchematicAtom.js
+++ js/view/InteractiveSchematicAtom.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2013, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Node that depicts an interactive atom where the user can add subatomic
