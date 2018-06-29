fc3e7132e682b9a05a2350013398f3ef239704cd
jbphet <john.blanco@colorado.edu>
2018-06-29 14:02:21 -0600

    fixed an issue with game level selection, see #187, did a bunch of code cleanup in the process

--- js/game/view/StartGameLevelNode.js
+++ js/game/view/StartGameLevelNode.js
@@ -1,8 +1,7 @@
 // Copyright 2013-2017, University of Colorado Boulder
 
 /**
- * Screen that is presented to the user that allows them to select from a set
- * of game levels to play.
+ * a Scenery Node that allows the user to select which game level to play
  *
  * @author John Blanco
  */
@@ -13,6 +12,7 @@ define( function( require ) {
   var BAAGameModel = require( 'BUILD_AN_ATOM/game/model/BAAGameModel' );
   var BAASharedConstants = require( 'BUILD_AN_ATOM/common/BAASharedConstants' );
   var buildAnAtom = require( 'BUILD_AN_ATOM/buildAnAtom' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
   var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var LevelSelectionButton = require( 'VEGAS/LevelSelectionButton' );
@@ -45,134 +45,106 @@ define( function( require ) {
 
     Node.call( this );
 
-    // Title
+    // title
     var title = new Text( chooseYourGameString, {
       font: new PhetFont( 30 ),
-      maxWidth: layoutBounds.width
+      maxWidth: layoutBounds.width,
+      centerX: layoutBounds.centerX
     } );
     this.addChild( title );
 
-    // Buttons for starting a game level.
-    var periodicTableGameButton = new LevelSelectionButton(
-      new Image( periodicTableIcon ),
-      gameModel.scores[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'periodic-table-game' ) ],
-      {
-        listener: function() {
-          gameModel.startGameLevel( 'periodic-table-game', tandem.createTandem( 'periodicTableGame' ) );
-        },
-        baseColor: BASE_COLOR,
-        bestTimeProperty: gameModel.bestTimes[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'periodic-table-game' ) ],
-        bestTimeVisibleProperty: gameModel.bestTimeVisible[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'periodic-table-game' ) ],
-        tandem: tandem.createTandem( 'periodicTableGameButton' ),
-        scoreDisplayOptions: {
-          numberOfStars: BAAGameModel.CHALLENGES_PER_LEVEL,
-          perfectScore: BAAGameModel.MAX_POINTS_PER_GAME_LEVEL
-        }
-      }
+    // buttons for starting a game level
+    var periodicTableGameButton = createLevelSelectionButton(
+      gameModel,
+      periodicTableIcon,
+      'periodic-table-game',
+      'periodicTableGame',
+      tandem
     );
-    this.addChild( periodicTableGameButton );
-    var massAndChargeGameButton = new LevelSelectionButton(
-      new Image( massChargeIcon ),
-      gameModel.scores[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'mass-and-charge-game' ) ],
-      {
-        listener: function() {
-          gameModel.startGameLevel( 'mass-and-charge-game', tandem.createTandem( 'massAndChargeGame' ) );
-        },
-        baseColor: BASE_COLOR,
-        bestTimeProperty: gameModel.bestTimes[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'mass-and-charge-game' ) ],
-        bestTimeVisibleProperty: gameModel.bestTimeVisible[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'mass-and-charge-game' ) ],
-        tandem: tandem.createTandem( 'massAndChargeGameButton' ),
-        scoreDisplayOptions: {
-          numberOfStars: BAAGameModel.CHALLENGES_PER_LEVEL,
-          perfectScore: BAAGameModel.MAX_POINTS_PER_GAME_LEVEL
-        }
-      }
+    var massAndChargeGameButton = createLevelSelectionButton(
+      gameModel,
+      massChargeIcon,
+      'mass-and-charge-game',
+      'massAndChargeGame',
+      tandem
     );
-    this.addChild( massAndChargeGameButton );
-    var symbolGameButton = new LevelSelectionButton(
-      new Image( symbolQuestionIcon ),
-      gameModel.scores[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'symbol-game' ) ],
-      {
-        listener: function() {
-          gameModel.startGameLevel( 'mass-and-charge-game', tandem.createTandem( 'symbolGame' ) );
-        },
-        baseColor: BASE_COLOR,
-        bestTimeProperty: gameModel.bestTimes[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'symbol-game' ) ],
-        bestTimeVisibleProperty: gameModel.bestTimeVisible[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'symbol-game' ) ],
-        tandem: tandem.createTandem( 'symbolGameButton' ),
-        scoreDisplayOptions: {
-          numberOfStars: BAAGameModel.CHALLENGES_PER_LEVEL,
-          perfectScore: BAAGameModel.MAX_POINTS_PER_GAME_LEVEL
-        }
-      }
+    var symbolGameButton = createLevelSelectionButton(
+      gameModel,
+      symbolQuestionIcon,
+      'symbol-game',
+      'symbolGame',
+      tandem
     );
-    this.addChild( symbolGameButton );
-    var advancedSymbolGameButton = new LevelSelectionButton(
-      new Image( questionMarkIcon ),
-      gameModel.scores[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'advanced-symbol-game' ) ],
-      {
-        listener: function() {
-          gameModel.startGameLevel( 'advanced-symbol-game', tandem.createTandem( 'advancedSymbolGame' ) );
-        },
-        baseColor: BASE_COLOR,
-        bestTimeProperty: gameModel.bestTimes[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'advanced-symbol-game' ) ],
-        bestTimeVisibleProperty: gameModel.bestTimeVisible[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( 'advanced-symbol-game' ) ],
-        tandem: tandem.createTandem( 'advancedSymbolGameButton' ),
-        scoreDisplayOptions: {
-          numberOfStars: BAAGameModel.CHALLENGES_PER_LEVEL,
-          perfectScore: BAAGameModel.MAX_POINTS_PER_GAME_LEVEL
-        }
-      }
+    var advancedSymbolGameButton = createLevelSelectionButton(
+      gameModel,
+      questionMarkIcon,
+      'advanced-symbol-game',
+      'advancedSymbolGame',
+      tandem
     );
-    this.addChild( advancedSymbolGameButton );
-
-    // Sound and timer controls.
-    var timerToggleButton = new TimerToggleButton( gameModel.timerEnabledProperty, {
-      stroke: 'gray',
-      tandem: tandem.createTandem( 'timerToggleButton' )
+    var buttonHBox = new HBox( {
+      children: [ periodicTableGameButton, massAndChargeGameButton, symbolGameButton, advancedSymbolGameButton ],
+      spacing: 30,
+      centerY: layoutBounds.centerY,
+      centerX: layoutBounds.centerX
     } );
-    this.addChild( timerToggleButton );
+    this.addChild( buttonHBox );
+
+    // sound and timer controls
     var soundToggleButton = new SoundToggleButton( gameModel.soundEnabledProperty, {
       stroke: 'gray',
-      tandem: tandem.createTandem( 'soundToggleButton' )
+      tandem: tandem.createTandem( 'soundToggleButton' ),
+      left: CONTROLS_INSET,
+      bottom: layoutBounds.height - CONTROLS_INSET
     } );
     this.addChild( soundToggleButton );
+    var timerToggleButton = new TimerToggleButton( gameModel.timerEnabledProperty, {
+      stroke: 'gray',
+      tandem: tandem.createTandem( 'timerToggleButton' ),
+      left: soundToggleButton.left,
+      bottom: soundToggleButton.top - 10
+    } );
+    this.addChild( timerToggleButton );
 
-    // Reset all button.
+    // reset all button
     var resetAllButton = new ResetAllButton( {
       listener: function() {
         gameModel.reset();
       },
       radius: BAASharedConstants.RESET_BUTTON_RADIUS,
       tandem: tandem.createTandem( 'resetAllButton' ),
-      touchAreaDilation: 8
+      touchAreaDilation: 8,
+      right: layoutBounds.width - CONTROLS_INSET,
+      bottom: layoutBounds.height - CONTROLS_INSET
     } );
     this.addChild( resetAllButton );
 
-    // Layout
-    var buttonWidth = periodicTableGameButton.width; // Note: Assumes all buttons are the same size.
-    var interButtonXSpace = buttonWidth * 0.2;
-    var buttonCenterY = layoutBounds.height * 0.5;
-    periodicTableGameButton.right = layoutBounds.centerX - 1.5 * interButtonXSpace - buttonWidth;
-    periodicTableGameButton.centerY = buttonCenterY;
-    massAndChargeGameButton.left = periodicTableGameButton.right + interButtonXSpace;
-    massAndChargeGameButton.centerY = buttonCenterY;
-    symbolGameButton.left = massAndChargeGameButton.right + interButtonXSpace;
-    symbolGameButton.centerY = buttonCenterY;
-    advancedSymbolGameButton.left = symbolGameButton.right + interButtonXSpace;
-    advancedSymbolGameButton.centerY = buttonCenterY;
-    resetAllButton.right = layoutBounds.width - CONTROLS_INSET;
-    resetAllButton.bottom = layoutBounds.height - CONTROLS_INSET;
-    title.centerX = layoutBounds.width / 2;
-    title.centerY = periodicTableGameButton.top / 2;
-    soundToggleButton.left = CONTROLS_INSET;
-    soundToggleButton.bottom = layoutBounds.height - CONTROLS_INSET;
-    timerToggleButton.left = CONTROLS_INSET;
-    timerToggleButton.bottom = soundToggleButton.top - 10;
+    // additional layout
+    title.centerY = ( layoutBounds.minY + buttonHBox.top ) / 2;
+  }
+
+  // helper function to create level selection buttons, helps to avoid code duplication
+  function createLevelSelectionButton( gameModel, icon, levelName, gameLevelTandemName, tandem ) {
+    return new LevelSelectionButton(
+      new Image( icon ),
+      gameModel.scores[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( levelName ) ],
+      {
+        listener: function() {
+          gameModel.startGameLevel( levelName, tandem.createTandem( gameLevelTandemName ) );
+        },
+        baseColor: BASE_COLOR,
+        bestTimeProperty: gameModel.bestTimes[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( levelName ) ],
+        bestTimeVisibleProperty: gameModel.bestTimeVisible[ ShredConstants.MAP_LEVEL_NAME_TO_NUMBER( levelName ) ],
+        tandem: tandem.createTandem( gameLevelTandemName + 'Button' ),
+        scoreDisplayOptions: {
+          numberOfStars: BAAGameModel.CHALLENGES_PER_LEVEL,
+          perfectScore: BAAGameModel.MAX_POINTS_PER_GAME_LEVEL
+        }
+      }
+    );
   }
 
   buildAnAtom.register( 'StartGameLevelNode', StartGameLevelNode );
 
-  // Inherit from Node.
   return inherit( Node, StartGameLevelNode );
 } );
