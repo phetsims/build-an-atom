84ff203c69d929e95eeeaa0521ed8ff6b6b8179b
zepumph <michael.kauzmann@colorado.edu>
2019-07-11 17:53:40 -0800

    make ?cacheBust QP a flag, https://github.com/phetsims/chipper/issues/767

--- js/build-an-atom-config.js
+++ js/build-an-atom-config.js
@@ -45,6 +45,6 @@ require.config( {
     VEGAS: '../../vegas/js'
   },
 
-  // optional cache bust to make browser refresh load all included scripts, can be disabled with ?cacheBust=false
+  // optional cache bust to make browser refresh load all included scripts, can be enabled with ?cacheBust
   urlArgs: phet.chipper.getCacheBustArgs()
 } );
