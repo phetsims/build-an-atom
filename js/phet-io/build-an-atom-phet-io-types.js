c922055a33fffe4b4643b6d5274e4e8c5d1f7d6a
zepumph <michael.kauzmann@colorado.edu>
2019-10-03 15:12:40 -0800

    ObjectIO.supertype === null, factor out getPrototypeOf usages to ObjectIO.getSupertype, https://github.com/phetsims/phet-io-wrappers/issues/298

--- js/phet-io/build-an-atom-phet-io-types.js
+++ js/phet-io/build-an-atom-phet-io-types.js
@@ -815,7 +815,7 @@ window.phet.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
-      "supertype": "Object",
+      "supertype": null,
       "typeName": "ObjectIO"
     },
     "ObservableArrayIO<ParticleIO>": {
