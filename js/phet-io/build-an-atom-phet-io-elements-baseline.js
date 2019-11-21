60973f5b076eb8b8d25aa3b5feafa0e59cf915ca
zepumph <michael.kauzmann@colorado.edu>
2019-11-20 16:02:55 -0900

    rename PhetioSingleton -> PhetioCapsule, https://github.com/phetsims/phet-io/issues/1567

--- js/phet-io/build-an-atom-phet-io-elements-baseline.js
+++ js/phet-io/build-an-atom-phet-io-elements-baseline.js
@@ -26779,7 +26779,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "PhetioSingletonIO<DialogIO>"
+      "phetioTypeName": "PhetioCapsuleIO<DialogIO>"
     },
     "buildAnAtom.general.navigationBar.phetButton.phetMenu.aboutDialogSingleton.prototype": {
       "phetioDocumentation": "",
