4116afa7293e2d4d481d480ed881b33e71586d70
zepumph <michael.kauzmann@colorado.edu>
2020-02-28 13:35:51 -0900

    update documentation, https://github.com/phetsims/studio/issues/103

--- js/phet-io/build-an-atom-phet-io-elements-baseline.js
+++ js/phet-io/build-an-atom-phet-io-elements-baseline.js
@@ -27641,7 +27641,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "buildAnAtom.general.soundManager.enhancedSoundEnabledProperty": {
-      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled.",
+      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled. Note that not all simulations that support sound also support enhanced sound.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
