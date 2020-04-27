b23d076b282a2d3f3b05c146737483262a0a3a7a
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:03 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/build-an-atom-baseline.js
+++ js/phet-io/build-an-atom-baseline.js
@@ -27645,7 +27645,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "buildAnAtom.general.view.simNameProperty": {
+    "buildAnAtom.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
