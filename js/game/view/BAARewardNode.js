e2defe4d1bdfc4be5f43138411d6879e6c0f543f
pixelzoom <cmalley@pixelzoom.com>
2020-09-17 00:16:54 -0600

    convert RewardDialog to ES6, https://github.com/phetsims/vegas/issues/82

--- js/game/view/BAARewardNode.js
+++ js/game/view/BAARewardNode.js
@@ -1,4 +1,5 @@
 // Copyright 2016-2020, University of Colorado Boulder
+
 /**
  * The reward that is displayed when a game is completed with a perfect score. For testing, the simulation can be run
  * with the 'reward' query parameter to show the reward at the end of every game, regardless of score.
@@ -6,7 +7,6 @@
  * @author Aadish Gupta
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import FaceNode from '../../../../scenery-phet/js/FaceNode.js';
 import AtomIdentifier from '../../../../shred/js/AtomIdentifier.js';
 import NumberAtom from '../../../../shred/js/model/NumberAtom.js';
@@ -21,61 +21,59 @@ const FACE_DIAMETER = 40;
 const MIN_CHILD_NODE_WIDTH = 40;
 const MAX_CHILD_NODE_WIDTH = MIN_CHILD_NODE_WIDTH * 2;
 
-/**
- * @param {Tandem} tandem
- * @constructor
- */
-function BAARewardNode( tandem ) {
-  const nodes = this.createNodes( tandem );
-  RewardNode.call( this, { nodes: nodes } );
+class BAARewardNode extends RewardNode {
 
-  // @private
-  this.disposeBAARewardNode = function() {
-    nodes.forEach( function( node ) {
-      !node.isDisposed && node.dispose();
-    } );
-  };
-}
+  /**
+   * @param {Tandem} tandem
+   */
+  constructor( tandem ) {
+    const nodes = createNodes( tandem );
+    super( { nodes: nodes } );
 
-buildAnAtom.register( 'BAARewardNode', BAARewardNode );
-
-inherit( RewardNode, BAARewardNode, {
+    // @private
+    this.disposeBAARewardNode = function() {
+      nodes.forEach( node => {
+        !node.isDisposed && node.dispose();
+      } );
+    };
+  }
 
   /**
    * @public
+   * @override
    */
-  dispose: function() {
+  dispose() {
     this.disposeBAARewardNode();
-    RewardNode.prototype.dispose.call( this );
-  },
-
-  // @private
-  _createRandomStableAtom: function() {
-    const atomicNumber = 1 + phet.joist.random.nextInt( 18 ); // Limit to Argon, since that's as high as translations go.
-    return new NumberAtom( {
-      protonCount: atomicNumber,
-      neutronCount: AtomIdentifier.getNumNeutronsInMostCommonIsotope( atomicNumber ),
-      electronCount: atomicNumber
-    } );
-  },
-
-  // @public
-  createNodes: function( tandem ) {
-    const self = this;
-    const nodes = [];
-    const groupTandem = tandem.createGroupTandem( 'interactiveSymbolNodes' );
-    for ( let i = 0; i < NUMBER_OF_SYMBOL_NODES; i++ ) {
-      const interactiveSymbolNode = new InteractiveSymbolNode( self._createRandomStableAtom(), groupTandem.createNextTandem() );
-      interactiveSymbolNode.scale( ( MIN_CHILD_NODE_WIDTH +
-                                     phet.joist.random.nextDouble() *
-                                     ( MAX_CHILD_NODE_WIDTH - MIN_CHILD_NODE_WIDTH ) ) /
-                                   interactiveSymbolNode.width );
-      nodes.push( interactiveSymbolNode );
-    }
-    const faceNode = new FaceNode( FACE_DIAMETER );
-    nodes.push( faceNode );
-    return RewardNode.createRandomNodes( nodes, NUMBER_OF_NODES );
+    super.dispose();
   }
-} );
+}
 
+// @private
+function createRandomStableAtom() {
+  const atomicNumber = 1 + phet.joist.random.nextInt( 18 ); // Limit to Argon, since that's as high as translations go.
+  return new NumberAtom( {
+    protonCount: atomicNumber,
+    neutronCount: AtomIdentifier.getNumNeutronsInMostCommonIsotope( atomicNumber ),
+    electronCount: atomicNumber
+  } );
+}
+
+// @public
+function createNodes( tandem ) {
+  const nodes = [];
+  const groupTandem = tandem.createGroupTandem( 'interactiveSymbolNodes' );
+  for ( let i = 0; i < NUMBER_OF_SYMBOL_NODES; i++ ) {
+    const interactiveSymbolNode = new InteractiveSymbolNode( createRandomStableAtom(), groupTandem.createNextTandem() );
+    interactiveSymbolNode.scale( ( MIN_CHILD_NODE_WIDTH +
+                                   phet.joist.random.nextDouble() *
+                                   ( MAX_CHILD_NODE_WIDTH - MIN_CHILD_NODE_WIDTH ) ) /
+                                 interactiveSymbolNode.width );
+    nodes.push( interactiveSymbolNode );
+  }
+  const faceNode = new FaceNode( FACE_DIAMETER );
+  nodes.push( faceNode );
+  return RewardNode.createRandomNodes( nodes, NUMBER_OF_NODES );
+}
+
+buildAnAtom.register( 'BAARewardNode', BAARewardNode );
 export default BAARewardNode;
