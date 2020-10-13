fc8106439f653121b968e68555bfd6392ad403cf
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 16:40:58 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/view/InteractiveSchematicAtom.js
+++ js/view/InteractiveSchematicAtom.js
@@ -4,7 +4,6 @@
  * Node that depicts an interactive atom where the user can add subatomic particles from a set of buckets.
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import BucketFront from '../../../scenery-phet/js/bucket/BucketFront.js';
 import BucketHole from '../../../scenery-phet/js/bucket/BucketHole.js';
@@ -18,151 +17,148 @@ import ParticleView from './ParticleView.js';
 // constants
 const NUM_NUCLEON_LAYERS = 5; // This is based on max number of particles, may need adjustment if that changes.
 
-/**
- * @param {BuildAnAtomModel} model
- * @param {ModelViewTransform2} modelViewTransform
- * @param {Object} [options]
- * @constructor
- */
-function InteractiveSchematicAtom( model, modelViewTransform, options ) {
-  options = merge( {
-    tandem: Tandem.REQUIRED
-  }, options );
+class InteractiveSchematicAtom extends Node {
 
-  Node.call( this );
-  const self = this;
+  /**
+   * @param {BuildAnAtomModel} model
+   * @param {ModelViewTransform2} modelViewTransform
+   * @param {Object} [options]
+   */
+  constructor( model, modelViewTransform, options ) {
+    options = merge( {
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  const particleViews = []; // remember all the particleViews when using in dispose
+    super();
 
-  // Add the node that depicts the textual labels, the electron shells, and the center X marker.
-  const atomNode = new AtomNode( model.particleAtom, modelViewTransform, {
-    showElementNameProperty: model.showElementNameProperty,
-    showNeutralOrIonProperty: model.showNeutralOrIonProperty,
-    showStableOrUnstableProperty: model.showStableOrUnstableProperty,
-    electronShellDepictionProperty: model.electronShellDepictionProperty,
-    tandem: options.tandem.createTandem( 'atomNode' )
-  } );
-  this.addChild( atomNode );
+    const particleViews = []; // remember all the particleViews when using in dispose
 
-  // Add the bucket holes.  Done separately from the bucket front for layering.
-  _.each( model.buckets, function( bucket ) {
-    self.addChild( new BucketHole( bucket, modelViewTransform ) );
-  } );
-
-  // Add the layers where the nucleons will be maintained.
-  const nucleonLayers = [];
-  _.times( NUM_NUCLEON_LAYERS, function() {
-    const nucleonLayer = new Node();
-    nucleonLayers.push( nucleonLayer );
-    self.addChild( nucleonLayer );
-  } );
-  nucleonLayers.reverse(); // Set up the nucleon layers so that layer 0 is in front.
-
-  // Add the layer where the electrons will be maintained.
-  const electronLayer = new Node( { layerSplit: true } );
-  this.addChild( electronLayer );
-
-  // Add the nucleon particle views.
-  const nucleonGroupTandem = options.tandem && options.tandem.createGroupTandem( 'nucleons' );
-  const electronGroupTandem = options.tandem && options.tandem.createGroupTandem( 'electrons' );
-  model.nucleons.forEach( function( nucleon ) {
-    const particleView = new ParticleView( nucleon, modelViewTransform, {
-      tandem: nucleonGroupTandem && nucleonGroupTandem.createNextTandem()
+    // Add the node that depicts the textual labels, the electron shells, and the center X marker.
+    const atomNode = new AtomNode( model.particleAtom, modelViewTransform, {
+      showElementNameProperty: model.showElementNameProperty,
+      showNeutralOrIonProperty: model.showNeutralOrIonProperty,
+      showStableOrUnstableProperty: model.showStableOrUnstableProperty,
+      electronShellDepictionProperty: model.electronShellDepictionProperty,
+      tandem: options.tandem.createTandem( 'atomNode' )
     } );
-    nucleonLayers[ nucleon.zLayerProperty.get() ].addChild( particleView );
-    particleViews.push( particleView );
+    this.addChild( atomNode );
 
-    // Add a listener that adjusts a nucleon's z-order layering.
-    nucleon.zLayerProperty.link( function( zLayer ) {
-      assert && assert( nucleonLayers.length > zLayer,
-        'zLayer for nucleon exceeds number of layers, max number may need increasing.' );
+    // Add the bucket holes.  Done separately from the bucket front for layering.
+    _.each( model.buckets, bucket => this.addChild( new BucketHole( bucket, modelViewTransform ) ) );
 
-      // Determine whether nucleon view is on the correct layer.
-      let onCorrectLayer = false;
-      nucleonLayers[ zLayer ].children.forEach( function( particleView ) {
-        if ( particleView.particle === nucleon ) {
-          onCorrectLayer = true;
-        }
+    // Add the layers where the nucleons will be maintained.
+    const nucleonLayers = [];
+    _.times( NUM_NUCLEON_LAYERS, () => {
+      const nucleonLayer = new Node();
+      nucleonLayers.push( nucleonLayer );
+      this.addChild( nucleonLayer );
+    } );
+    nucleonLayers.reverse(); // Set up the nucleon layers so that layer 0 is in front.
+
+    // Add the layer where the electrons will be maintained.
+    const electronLayer = new Node( { layerSplit: true } );
+    this.addChild( electronLayer );
+
+    // Add the nucleon particle views.
+    const nucleonGroupTandem = options.tandem && options.tandem.createGroupTandem( 'nucleons' );
+    const electronGroupTandem = options.tandem && options.tandem.createGroupTandem( 'electrons' );
+    model.nucleons.forEach( nucleon => {
+      const particleView = new ParticleView( nucleon, modelViewTransform, {
+        tandem: nucleonGroupTandem && nucleonGroupTandem.createNextTandem()
       } );
+      nucleonLayers[ nucleon.zLayerProperty.get() ].addChild( particleView );
+      particleViews.push( particleView );
 
-      if ( !onCorrectLayer ) {
+      // Add a listener that adjusts a nucleon's z-order layering.
+      nucleon.zLayerProperty.link( zLayer => {
+        assert && assert( nucleonLayers.length > zLayer,
+          'zLayer for nucleon exceeds number of layers, max number may need increasing.' );
 
-        // Remove particle view from its current layer.
-        let particleView = null;
-        for ( let layerIndex = 0; layerIndex < nucleonLayers.length && particleView === null; layerIndex++ ) {
-          for ( let childIndex = 0; childIndex < nucleonLayers[ layerIndex ].children.length; childIndex++ ) {
-            if ( nucleonLayers[ layerIndex ].children[ childIndex ].particle === nucleon ) {
-              particleView = nucleonLayers[ layerIndex ].children[ childIndex ];
-              nucleonLayers[ layerIndex ].removeChildAt( childIndex );
-              break;
+        // Determine whether nucleon view is on the correct layer.
+        let onCorrectLayer = false;
+        nucleonLayers[ zLayer ].children.forEach( particleView => {
+          if ( particleView.particle === nucleon ) {
+            onCorrectLayer = true;
+          }
+        } );
+
+        if ( !onCorrectLayer ) {
+
+          // Remove particle view from its current layer.
+          let particleView = null;
+          for ( let layerIndex = 0; layerIndex < nucleonLayers.length && particleView === null; layerIndex++ ) {
+            for ( let childIndex = 0; childIndex < nucleonLayers[ layerIndex ].children.length; childIndex++ ) {
+              if ( nucleonLayers[ layerIndex ].children[ childIndex ].particle === nucleon ) {
+                particleView = nucleonLayers[ layerIndex ].children[ childIndex ];
+                nucleonLayers[ layerIndex ].removeChildAt( childIndex );
+                break;
+              }
             }
           }
+
+          // Add the particle view to its new layer.
+          assert && assert( particleView !== null, 'Particle view not found during relayering' );
+          nucleonLayers[ zLayer ].addChild( particleView );
         }
-
-        // Add the particle view to its new layer.
-        assert && assert( particleView !== null, 'Particle view not found during relayering' );
-        nucleonLayers[ zLayer ].addChild( particleView );
-      }
+      } );
     } );
-  } );
 
-  // Add the electron particle views.
-  model.electrons.forEach( function( electron ) {
-    const particleView = new ParticleView( electron, modelViewTransform, {
-      tandem: electronGroupTandem.createNextTandem()
+    // Add the electron particle views.
+    model.electrons.forEach( electron => {
+      const particleView = new ParticleView( electron, modelViewTransform, {
+        tandem: electronGroupTandem.createNextTandem()
+      } );
+      electronLayer.addChild( particleView );
+      particleViews.push( particleView );
     } );
-    electronLayer.addChild( particleView );
-    particleViews.push( particleView );
-  } );
 
-  // When the electrons are represented as a cloud, the individual particles become invisible when added to the atom.
-  const updateElectronVisibility = function() {
-    electronLayer.getChildren().forEach( function( electronNode ) {
-      electronNode.visible = model.electronShellDepictionProperty.get() === 'orbits' || !model.particleAtom.electrons.includes( electronNode.particle );
+    // When the electrons are represented as a cloud, the individual particles become invisible when added to the atom.
+    const updateElectronVisibility = () => {
+      electronLayer.getChildren().forEach( electronNode => {
+        electronNode.visible = model.electronShellDepictionProperty.get() === 'orbits' ||
+                               !model.particleAtom.electrons.includes( electronNode.particle );
+      } );
+    };
+    model.particleAtom.electrons.lengthProperty.link( updateElectronVisibility );
+    model.electronShellDepictionProperty.link( updateElectronVisibility );
+
+    // Add the front portion of the buckets. This is done separately from the bucket holes for layering purposes.
+    const bucketGroupTandem = options.tandem.createGroupTandem( 'bucketFronts' );
+    const bucketFrontsAndDragHandlers = []; // keep track for disposal
+    _.each( model.buckets, bucket => {
+      const bucketFront = new BucketFront( bucket, modelViewTransform, { tandem: bucketGroupTandem.createNextTandem() } );
+      this.addChild( bucketFront );
+      const bucketDragListener = new BucketDragListener( bucket, bucketFront, modelViewTransform, {
+        tandem: options.tandem && options.tandem.createTandem( bucket.sphereBucketTandem.name + 'DragListener' )
+      } );
+      bucketFront.addInputListener( bucketDragListener );
+
+      // add to separate list for later disposal
+      bucketFrontsAndDragHandlers.push( bucketFront );
+      bucketFrontsAndDragHandlers.push( bucketDragListener );
     } );
-  };
-  model.particleAtom.electrons.lengthProperty.link( updateElectronVisibility );
-  model.electronShellDepictionProperty.link( updateElectronVisibility );
 
-  // Add the front portion of the buckets. This is done separately from the bucket holes for layering purposes.
-  const bucketGroupTandem = options.tandem.createGroupTandem( 'bucketFronts' );
-  const bucketFrontsAndDragHandlers = []; // keep track for disposal
-  _.each( model.buckets, function( bucket ) {
-    const bucketFront = new BucketFront( bucket, modelViewTransform, { tandem: bucketGroupTandem.createNextTandem() } );
-    self.addChild( bucketFront );
-    const bucketDragListener = new BucketDragListener( bucket, bucketFront, modelViewTransform, {
-      tandem: options.tandem && options.tandem.createTandem( bucket.sphereBucketTandem.name + 'DragListener' )
-    } );
-    bucketFront.addInputListener( bucketDragListener );
+    // @private
+    this.disposeInteractiveSchematicAtom = () => {
+      particleViews.forEach( particleView => particleView.dispose() );
+      bucketFrontsAndDragHandlers.forEach( bucketItem => bucketItem.dispose() );
+      atomNode.dispose();
+      model.particleAtom.electrons.lengthProperty.unlink( updateElectronVisibility );
+      model.electronShellDepictionProperty.unlink( updateElectronVisibility );
+    };
 
-    // add to separate list for later disposal
-    bucketFrontsAndDragHandlers.push( bucketFront );
-    bucketFrontsAndDragHandlers.push( bucketDragListener );
-  } );
-
-  this.disposeInteractiveSchematicAtom = function() {
-    particleViews.forEach( function( particleView ) { particleView.dispose(); } );
-    bucketFrontsAndDragHandlers.forEach( function( bucketItem ) { bucketItem.dispose(); } );
-    atomNode.dispose();
-    model.particleAtom.electrons.lengthProperty.unlink( updateElectronVisibility );
-    model.electronShellDepictionProperty.unlink( updateElectronVisibility );
-  };
-
-  this.mutate( options );
-}
-
-shred.register( 'InteractiveSchematicAtom', InteractiveSchematicAtom );
-
-inherit( Node, InteractiveSchematicAtom, {
+    this.mutate( options );
+  }
 
   /**
    * @public
    * @override
    */
-  dispose: function() {
+  dispose() {
     this.disposeInteractiveSchematicAtom();
-    Node.prototype.dispose.call( this );
+    super.dispose();
   }
-} );
+}
 
+shred.register( 'InteractiveSchematicAtom', InteractiveSchematicAtom );
 export default InteractiveSchematicAtom;
