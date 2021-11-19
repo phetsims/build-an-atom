7098a42b973409fb38b03320bd0e24e98e325e18
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:00 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/buildAnAtomStrings.ts
@@ -0,0 +1,49 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import buildAnAtom from './buildAnAtom.js';
+
+type StringsType = {
+  'ion': string,
+  'stableSlashUnstable': string,
+  'symbol': string,
+  'findTheElement': string,
+  'massNumber': string,
+  'atom': string,
+  'netCharge': string,
+  'element': string,
+  'orbits': string,
+  'model': string,
+  'whatIsTheMassNumber': string,
+  'build-an-atom': {
+    'title': string
+  },
+  'whatIsTheTotalCharge': string,
+  'electronsColon': string,
+  'electronsColonPattern': string,
+  'game': string,
+  'neutralAtom': string,
+  'neutralSlashIon': string,
+  'neutrons': string,
+  'protonsColon': string,
+  'protonsColonPattern': string,
+  'cloud': string,
+  'neutronsColon': string,
+  'neutronsColonPattern': string,
+  'electrons': string,
+  'isIt': string,
+  'protons': string,
+  'show': string,
+  'chooseYourGame': string,
+  'highContrastParticles': string
+};
+
+const buildAnAtomStrings = getStringModule( 'BUILD_AN_ATOM' ) as StringsType;
+
+buildAnAtom.register( 'buildAnAtomStrings', buildAnAtomStrings );
+
+export default buildAnAtomStrings;
