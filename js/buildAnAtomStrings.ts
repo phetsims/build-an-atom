876eed270f0d6d48c865bbaeda07a2ae30990f93
Sam Reid <reids@colorado.edu>
2022-05-07 09:35:38 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/buildAnAtomStrings.ts
+++ js/buildAnAtomStrings.ts
@@ -8,38 +8,38 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import buildAnAtom from './buildAnAtom.js';
 
 type StringsType = {
-  'ion': string,
-  'stableSlashUnstable': string,
-  'symbol': string,
-  'findTheElement': string,
-  'massNumber': string,
-  'atom': string,
-  'netCharge': string,
-  'element': string,
-  'orbits': string,
-  'model': string,
-  'whatIsTheMassNumber': string,
+  'ion': string;
+  'stableSlashUnstable': string;
+  'symbol': string;
+  'findTheElement': string;
+  'massNumber': string;
+  'atom': string;
+  'netCharge': string;
+  'element': string;
+  'orbits': string;
+  'model': string;
+  'whatIsTheMassNumber': string;
   'build-an-atom': {
-    'title': string
-  },
-  'whatIsTheTotalCharge': string,
-  'electronsColon': string,
-  'electronsColonPattern': string,
-  'game': string,
-  'neutralAtom': string,
-  'neutralSlashIon': string,
-  'neutrons': string,
-  'protonsColon': string,
-  'protonsColonPattern': string,
-  'cloud': string,
-  'neutronsColon': string,
-  'neutronsColonPattern': string,
-  'electrons': string,
-  'isIt': string,
-  'protons': string,
-  'show': string,
-  'chooseYourGame': string,
-  'highContrastParticles': string
+    'title': string;
+  };
+  'whatIsTheTotalCharge': string;
+  'electronsColon': string;
+  'electronsColonPattern': string;
+  'game': string;
+  'neutralAtom': string;
+  'neutralSlashIon': string;
+  'neutrons': string;
+  'protonsColon': string;
+  'protonsColonPattern': string;
+  'cloud': string;
+  'neutronsColon': string;
+  'neutronsColonPattern': string;
+  'electrons': string;
+  'isIt': string;
+  'protons': string;
+  'show': string;
+  'chooseYourGame': string;
+  'highContrastParticles': string;
 };
 
 const buildAnAtomStrings = getStringModule( 'BUILD_AN_ATOM' ) as StringsType;
