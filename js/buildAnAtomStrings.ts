427213fc7289eee2e826b973054b37df5f4dd81c
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:39 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/buildAnAtomStrings.ts
+++ js/buildAnAtomStrings.ts
@@ -5,41 +5,72 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import buildAnAtom from './buildAnAtom.js';
 
 type StringsType = {
   'ion': string;
+  'ionProperty': TReadOnlyProperty<string>;
   'stableSlashUnstable': string;
+  'stableSlashUnstableProperty': TReadOnlyProperty<string>;
   'symbol': string;
+  'symbolProperty': TReadOnlyProperty<string>;
   'findTheElement': string;
+  'findTheElementProperty': TReadOnlyProperty<string>;
   'massNumber': string;
+  'massNumberProperty': TReadOnlyProperty<string>;
   'atom': string;
+  'atomProperty': TReadOnlyProperty<string>;
   'netCharge': string;
+  'netChargeProperty': TReadOnlyProperty<string>;
   'element': string;
+  'elementProperty': TReadOnlyProperty<string>;
   'orbits': string;
+  'orbitsProperty': TReadOnlyProperty<string>;
   'model': string;
+  'modelProperty': TReadOnlyProperty<string>;
   'whatIsTheMassNumber': string;
+  'whatIsTheMassNumberProperty': TReadOnlyProperty<string>;
   'build-an-atom': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'whatIsTheTotalCharge': string;
+  'whatIsTheTotalChargeProperty': TReadOnlyProperty<string>;
   'electronsColon': string;
+  'electronsColonProperty': TReadOnlyProperty<string>;
   'electronsColonPattern': string;
+  'electronsColonPatternProperty': TReadOnlyProperty<string>;
   'game': string;
+  'gameProperty': TReadOnlyProperty<string>;
   'neutralAtom': string;
+  'neutralAtomProperty': TReadOnlyProperty<string>;
   'neutralSlashIon': string;
+  'neutralSlashIonProperty': TReadOnlyProperty<string>;
   'neutrons': string;
+  'neutronsProperty': TReadOnlyProperty<string>;
   'protonsColon': string;
+  'protonsColonProperty': TReadOnlyProperty<string>;
   'protonsColonPattern': string;
+  'protonsColonPatternProperty': TReadOnlyProperty<string>;
   'cloud': string;
+  'cloudProperty': TReadOnlyProperty<string>;
   'neutronsColon': string;
+  'neutronsColonProperty': TReadOnlyProperty<string>;
   'neutronsColonPattern': string;
+  'neutronsColonPatternProperty': TReadOnlyProperty<string>;
   'electrons': string;
+  'electronsProperty': TReadOnlyProperty<string>;
   'isIt': string;
+  'isItProperty': TReadOnlyProperty<string>;
   'protons': string;
+  'protonsProperty': TReadOnlyProperty<string>;
   'show': string;
+  'showProperty': TReadOnlyProperty<string>;
   'chooseYourGame': string;
+  'chooseYourGameProperty': TReadOnlyProperty<string>;
   'highContrastParticles': string;
+  'highContrastParticlesProperty': TReadOnlyProperty<string>;
 };
 
 const buildAnAtomStrings = getStringModule( 'BUILD_AN_ATOM' ) as StringsType;
