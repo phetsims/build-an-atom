4b1705f0056fc6057fca37d21159aff30f675e7b
phet-dev <phetadmin@gmail.com>
2022-11-11 03:39:23 -0700

    update copyright dates from daily grunt work

--- js/game/model/BAAGameState.js
+++ js/game/model/BAAGameState.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * Base type for Build and Atom game states.  These states use the Object Oriented state pattern, please see
