44080ada0186c475c4b474a47dba42e58223cd58
phet-dev <phetadmin@gmail.com>
2024-12-06 03:19:29 -0700

    update copyright dates from daily grunt work

--- js/common/model/BuildAnAtomModel.js
+++ js/common/model/BuildAnAtomModel.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2024, University of Colorado Boulder
 
 /**
  * A model of a set of subatomic particles - protons, neutrons, and electrons - that can be assembled into atoms.
