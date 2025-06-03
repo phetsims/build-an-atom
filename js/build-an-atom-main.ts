6afbb1ff39b5394730d4a1935182e9a2fdb26b23
AgustinVallejo <agustin.vallejo@udea.edu.co>
2025-06-03 12:17:41 -0500

    Converting main file to TypeScript https://github.com/phetsims/build-an-atom/issues/241

--- js/build-an-atom-main.ts
+++ js/build-an-atom-main.ts
@@ -7,7 +7,7 @@
  */
 
 import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
-import Sim from '../../joist/js/Sim.js';
+import Sim, { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import AtomScreen from './atom/AtomScreen.js';
@@ -22,37 +22,38 @@ const buildAnAtomTitleStringProperty = BuildAnAtomStrings[ 'build-an-atom' ].tit
 // root tandem
 const tandem = Tandem.ROOT;
 
-const simOptions = {
-  credits: {
-    leadDesign: 'Kelly Lancaster',
-    softwareDevelopment: 'John Blanco, Aadish Gupta, Sam Reid',
-    team: 'Jack Barbera, Suzanne Brahmia, Julia Chamberlain, Yuen-ying Carpenter, ' +
-          'Kelly Lancaster, Patricia Loeblein, Emily B. Moore, Robert Parson, ' +
-          'Ariel Paul, Kathy Perkins, Sharon Siman-Tov',
-    qualityAssurance: 'Steele Dalton, Alex Dornan, Bryce Griebenow, Ethan Johnson, ' +
-                      'Elise Morgan, Ben Roberts',
-    thanks: 'Conversion of this simulation to HTML5 was funded by the Royal Society of Chemistry.'
-  },
-
-  preferencesModel: new PreferencesModel( {
-    visualOptions: {
-      customPreferences: [ {
-        createContent: tandem => new VisualPreferencesNode( BAAGlobalPreferences.highContrastParticlesProperty,
-          tandem.createTandem( 'simPreferences' ) )
-      } ]
-    }
-  } )
-};
-
 simLauncher.launch( () => {
   const screens = [
     new AtomScreen( tandem.createTandem( 'atomScreen' ) ),
     new SymbolScreen( tandem.createTandem( 'symbolScreen' ) )
   ];
 
+  const options: SimOptions = {
+
+    credits: {
+      leadDesign: 'Kelly Lancaster',
+      softwareDevelopment: 'John Blanco, Aadish Gupta, Sam Reid',
+      team: 'Jack Barbera, Suzanne Brahmia, Julia Chamberlain, Yuen-ying Carpenter, ' +
+            'Kelly Lancaster, Patricia Loeblein, Emily B. Moore, Robert Parson, ' +
+            'Ariel Paul, Kathy Perkins, Sharon Siman-Tov',
+      qualityAssurance: 'Steele Dalton, Alex Dornan, Bryce Griebenow, Ethan Johnson, ' +
+                        'Elise Morgan, Ben Roberts',
+      thanks: 'Conversion of this simulation to HTML5 was funded by the Royal Society of Chemistry.'
+    },
+
+    preferencesModel: new PreferencesModel( {
+      visualOptions: {
+        customPreferences: [ {
+          createContent: tandem => new VisualPreferencesNode( BAAGlobalPreferences.highContrastParticlesProperty,
+            tandem.createTandem( 'simPreferences' ) )
+        } ]
+      }
+    } )
+  };
+
   // PhET-iO does not support the game screen (yet), see https://github.com/phetsims/build-an-atom/issues/156
   if ( !Tandem.PHET_IO_ENABLED ) {
     screens.push( new GameScreen( tandem.createTandem( 'gameScreen' ) ) );
   }
-  new Sim( buildAnAtomTitleStringProperty, screens, simOptions ).start();
+  new Sim( buildAnAtomTitleStringProperty, screens, options ).start();
 } );
