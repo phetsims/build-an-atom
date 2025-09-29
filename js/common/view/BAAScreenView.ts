cba136b5bc8fb121e540be900c7d11b8917e2795
jbphet <jpblanco@yahoo.com>
2025-09-29 12:13:32 -0600

    add initial cut at behavior to move particles from buckets to atoms using alt input, see https://github.com/phetsims/build-an-atom/issues/356
    # Conflicts:
    #	js/common/view/BAAScreenView.ts

--- js/common/view/BAAScreenView.ts
+++ js/common/view/BAAScreenView.ts
@@ -10,12 +10,14 @@
  */
 
 import DerivedStringProperty from '../../../../axon/js/DerivedStringProperty.js';
+import Multilink from '../../../../axon/js/Multilink.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import ScreenView, { ScreenViewOptions } from '../../../../joist/js/ScreenView.js';
 import Shape from '../../../../kite/js/Shape.js';
 import affirm from '../../../../perennial-alias/js/browser-and-node/affirm.js';
 import { combineOptions } from '../../../../phet-core/js/optionize.js';
+import { ParticleContainer } from '../../../../phetcommon/js/model/ParticleContainer.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import AccessibleInteractiveOptions from '../../../../scenery-phet/js/accessibility/AccessibleInteractiveOptions.js';
@@ -91,7 +93,8 @@ class BAAScreenView extends ScreenView {
       electronShellDepictionProperty: this.viewProperties.electronModelProperty,
       tandem: tandem.createTandem( 'atomNode' ),
       phetioVisiblePropertyInstrumented: false,
-      phetioFeatured: true
+      phetioFeatured: true,
+      focusable: false
     }, AccessibleInteractiveOptions ) );
     this.addChild( atomNode );
 
@@ -110,109 +113,78 @@ class BAAScreenView extends ScreenView {
       } ) );
     } );
 
-    // Add the layer where the nucleons and electrons will go, this is added last so that it remains on top.
-    const nucleonElectronLayer = new Node();
+    // Add the layer where the nucleons and electrons will go.
+    const particleLayer = new Node();
 
-    // Add the layers where the nucleons will exist.  The lower the index number, the closer to the front.  Layer 0 is
-    // reserved for nucleons that are being dragged.
-    const nucleonLayers: Node[] = [];
-    _.times( BAAModel.NUMBER_OF_NUCLEON_LAYERS, () => {
-      const nucleonLayer = new Node();
-      nucleonLayers.push( nucleonLayer );
-      nucleonElectronLayer.addChild( nucleonLayer );
-    } );
-    nucleonLayers.reverse(); // Set up the nucleon layers so that layer 0 is in front.
-
-    // Add the layer where the electrons will exist.
-    const electronLayer = new Node();
-    nucleonElectronLayer.addChild( electronLayer );
-
-    // Add the nucleon particle views.
     const protonsGroupTandem = tandem.createTandem( 'protonNodes' ).createGroupTandem( 'protonNode', 1 );
     const neutronsGroupTandem = tandem.createTandem( 'neutronNodes' ).createGroupTandem( 'neutronNode', 1 );
     const electronsGroupTandem = tandem.createTandem( 'electronNodes' ).createGroupTandem( 'electronNode', 1 );
 
-    // Add the nucleons.
     const particleDragBounds = modelViewTransform.viewToModelBounds( this.layoutBounds );
 
-    model.nucleons.forEach( nucleon => {
+    // Create a map that associates particles with their views for quick lookup.
+    const mapParticlesToViews: Map<Particle, ParticleView> = new Map<Particle, ParticleView>();
 
-      assert && assert( nucleon.type === 'proton' || nucleon.type === 'neutron', 'this is not a nucleon' );
+    // Add the particle views.
+    [ ...model.nucleons, ...model.electrons ].forEach( particle => {
 
-      nucleonLayers[ nucleon.zLayerProperty.value ].addChild( new BAAParticleView( nucleon, modelViewTransform, {
+      const particleView = new BAAParticleView( particle, modelViewTransform, {
         dragBounds: particleDragBounds,
         focusable: false,
-        tandem: nucleon.type === 'proton' ?
+        tandem: particle.type === 'proton' ?
                 protonsGroupTandem.createNextTandem() :
-                neutronsGroupTandem.createNextTandem()
-      } ) );
-
-      // Add a listener that adjusts a nucleon's z-order layering.
-      nucleon.zLayerProperty.link( zLayer => {
-        assert && assert(
-          nucleonLayers.length > zLayer,
-          'zLayer for nucleon exceeds number of layers, max number may need increasing.'
-        );
-
-        // Determine whether nucleon view is on the correct layer.
-        let onCorrectLayer = false;
-        nucleonLayers[ zLayer ].children.forEach( particleView => {
-          affirm( particleView instanceof ParticleView, 'Nucleon layers should only contain ParticleView instance' );
-          if ( particleView.particle === nucleon ) {
-            onCorrectLayer = true;
-          }
-        } );
-
-        if ( !onCorrectLayer ) {
-
-          // Remove particle view from its current layer.
-          let particleView = null;
-          for ( let layerIndex = 0; layerIndex < nucleonLayers.length && particleView === null; layerIndex++ ) {
-            for ( let childIndex = 0; childIndex < nucleonLayers[ layerIndex ].children.length; childIndex++ ) {
-              const nucleonChild = nucleonLayers[ layerIndex ].children[ childIndex ];
-
-              affirm( nucleonChild instanceof ParticleView, 'nucleonLayers should only contain ParticleView instances' );
-              if ( nucleonChild.particle === nucleon ) {
-                particleView = nucleonLayers[ layerIndex ].children[ childIndex ];
-                nucleonLayers[ layerIndex ].removeChildAt( childIndex );
-                break;
-              }
-            }
-          }
-
-          // Add the particle view to its new layer.
-          affirm( particleView !== null, 'Particle view not found during re-layering' );
-          nucleonLayers[ zLayer ].addChild( particleView );
-        }
+                particle.type === 'neutron' ?
+                neutronsGroupTandem.createNextTandem() :
+                electronsGroupTandem.createNextTandem()
       } );
+
+      particleLayer.addChild( particleView );
+      mapParticlesToViews.set( particle, particleView );
+
+      const bucketsAsParticleContainers: ParticleContainer<Particle>[] = model.buckets;
+
+      // The particle view will either be a child of this screen view or a child of the atom node, based on its state.
+      // The following listener moves it back and forth as needed.  It's necessary to change parent nodes like this to
+      // support alt-input group behavior in the atom node.
+      Multilink.multilink(
+        [ particle.containerProperty, particle.isDraggingProperty ],
+        ( container, isDragging ) => {
+
+          const isParticleNodeChildOfScreenView = particleView.parent === particleLayer;
+
+          if ( isParticleNodeChildOfScreenView && ( container === model.atom || isDragging ) ) {
+
+            // Move the particle view to the atom node.
+            particleLayer.removeChild( particleView );
+            atomNode.addParticleView( particleView );
+          }
+          else if ( !isParticleNodeChildOfScreenView &&
+                    ( ( container !== null && bucketsAsParticleContainers.includes( container ) ) ||
+                      ( container === null && !isDragging ) ) ) {
+
+            // Move the particle view to this screen view.
+            atomNode.removeParticleView( particleView );
+            particleLayer.addChild( particleView );
+          }
+        }
+      );
     } );
 
-    // Add the electron particle views.
-    model.electrons.forEach( ( electron: Particle ) => {
-      const electronNode = new BAAParticleView( electron, modelViewTransform, {
-        dragBounds: particleDragBounds,
-        focusable: false,
-        tandem: electronsGroupTandem.createNextTandem()
-      } );
-      electronLayer.addChild( electronNode );
-
-      electron.zLayerProperty.link( zLayer => {
-        if ( zLayer === 0 ) {
-          electronNode.moveToFront();
-        }
-      } );
-    } );
-
-    // When the electrons are represented as a cloud, the individual particles become invisible when added to the atom.
-    const updateElectronVisibility = () => {
-      electronLayer.getChildren().forEach( electronNode => {
-        affirm( electronNode instanceof ParticleView, 'electronLayer should only contain ParticleView instances' );
-        electronNode.visible = this.viewProperties.electronModelProperty.value === 'shells' ||
-                               !model.atom.electrons.includes( electronNode.particle );
+    // The following code manages the visibility of the individual electron particles.  When the electrons are
+    // represented as a cloud, the individual particles become invisible when added to the atom, but remain visible when
+    // outside the atom.
+    const updateElectronViewVisibility = () => {
+      model.electrons.forEach( electron => {
+        const electronView = mapParticlesToViews.get( electron );
+        affirm( electronView, 'Missing ParticleView for electron' );
+        const isElectronInAtom = model.atom.electrons.includes( electron );
+        electronView.visible = this.viewProperties.electronModelProperty.value === 'shells' ||
+                               electron.isDraggingProperty.value ||
+                               !isElectronInAtom;
       } );
     };
-    model.atom.electrons.lengthProperty.link( updateElectronVisibility );
-    this.viewProperties.electronModelProperty.link( updateElectronVisibility );
+    model.atom.electrons.lengthProperty.link( updateElectronViewVisibility );
+    this.viewProperties.electronModelProperty.link( updateElectronViewVisibility );
 
     // Add the front portion of the buckets. This is done separately from the bucket holes for layering purposes.
     const bucketFrontLayer = new Node();
@@ -253,12 +225,42 @@ class BAAScreenView extends ScreenView {
                  Vector2.ZERO;
         }
       } ) );
+
+      // Define a function that will find the view for a given particle.  Returns null if not found.
+      const findParticleView = ( particle: Particle ): ParticleView | null => {
+        let particleView = null;
+        for ( const node of particleLayer.children ) {
+          if ( node instanceof ParticleView && node.particle === particle ) {
+            particleView = node;
+            break;
+          }
+        }
+        if ( !particleView ) {
+          particleView = atomNode.getParticleView( particle );
+        }
+        return particleView;
+      };
+
+      // Add an input listener for accessibility that will be fired when the user presses enter or space while the
+      // bucket has focus.  This will extract a particle from the bucket and add it to the atom.
       bucketFront.addInputListener( {
         click: () => {
           const particle = bucket.extractClosestParticle( model.atom.positionProperty.value );
           if ( particle !== null ) {
-            model.atom.addParticle( particle );
-            atomNode.focus();
+
+            // Mark the particle as being controlled by the user via keyboard interaction.
+            particle.isDraggingProperty.value = true;
+
+            // Position the particle just below the center of the atom.
+            particle.setPositionAndDestination( model.atom.positionProperty.value.plusXY( 0, -40 ) );
+
+            // Get the view node that is associated with this particle.
+            const particleView = findParticleView( particle );
+            affirm( particleView !== null, 'ParticleView not found for extracted particle' );
+
+            // Set the focus to the particle's view so that it can be manipulated via keyboard.
+            particleView.focusable = true;
+            particleView.focus();
           }
         }
       } );
@@ -419,7 +421,7 @@ class BAAScreenView extends ScreenView {
     electronModelControl.left = atomNode.centerX + 130;
     electronModelControl.bottom = atomNode.bottom + 5;
 
-    this.addChild( nucleonElectronLayer );
+    this.addChild( particleLayer );
     this.addChild( bucketFrontLayer );
 
     // pdom - set navigation order for the Atom screen view
