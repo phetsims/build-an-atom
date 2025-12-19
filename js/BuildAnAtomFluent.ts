5b70daa281077e7401f86c6fdcc17de33a27a32a
Jesse <jesse.greenberg@colorado.edu>
2025-12-19 15:54:34 -0500

    Run modulify after change for https://github.com/phetsims/chipper/issues/1658

--- js/BuildAnAtomFluent.ts
+++ js/BuildAnAtomFluent.ts
@@ -11,6 +11,7 @@ import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
 import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
 import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
 import FluentComment from '../../chipper/js/browser/FluentComment.js';
+import FluentLibrary from '../../chipper/js/browser-and-node/FluentLibrary.js';
 import buildAnAtom from './buildAnAtom.js';
 import BuildAnAtomStrings from './BuildAnAtomStrings.js';
 
@@ -194,7 +195,7 @@ addToMapIfDefined( 'a11y_preferences_signLast_accessibleName', 'a11y.preferences
 const createFluentFile = (): string => {
   let ftl = '';
   for (const [key, stringProperty] of fluentKeyToStringPropertyMap.entries()) {
-    ftl += `${key} = ${stringProperty.value.replace('\n','\n ')}\n`;
+    ftl += `${key} = ${FluentLibrary.formatMultilineForFtl( stringProperty.value )}\n`;
   }
   return ftl;
 };
