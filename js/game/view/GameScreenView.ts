01afb37ff4a3e1b1fc65dd3fd77a2f974179b55c
jbphet <jpblanco@yahoo.com>
2025-12-29 14:57:53 -0700

    minor formatting fix

--- js/game/view/GameScreenView.ts
+++ js/game/view/GameScreenView.ts
@@ -192,7 +192,8 @@ class GameScreenView extends ScreenView {
           gameModel.timer.elapsedTimeProperty.value,
           level.bestTimeProperty.value === 0 ? null : level.bestTimeProperty.value,
           level.isNewBestTimeProperty.value,
-          () => { gameModel.startOver(); }, {
+          () => { gameModel.startOver(); },
+          {
             centerX: this.layoutBounds.width / 2,
             centerY: this.layoutBounds.height / 2,
             levelVisible: false,
